f20f3ae87504cc92f02740cb2cf4fac2950708b2
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-01-18 20:38:34 -0300

    crea alert dinamica

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react'
 import Boton from './components/Boton'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App () {
-  // Estados de los inputs
   const [nombre, setNombre] = useState( '' )
   const [contrasena, setContrasena] = useState( '' )
-  const [error, setError] = useState( false )
-  const [mensaje, setMensaje] = useState( '' )
-  const [active, setActive] = useState( false )
+  const [alerta, setAlerta] = useState( false ) // Estado para el error
+  const [mensaje, setMensaje] = useState( '' ) // Estado para el mensaje de error
+  const [active, setActive] = useState( false ) // Estado para el botón
+  const [color, setColor] = useState( 'alert alert-danger' )
 
   const handleButton = () => {
     if ( nombre !== '' && contrasena !== '' ) {
@@ -20,34 +20,25 @@ function App () {
 
   const validarDatos = ( e ) => {
     e.preventDefault()
-
-    if ( nombre === '' || contrasena === '' ) {
-      setError( true )
-      setMensaje( '⚠️ Todos los campos son obligatorios' )
-
-      return
-    }
-
     if ( nombre !== 'ADL' && contrasena !== '252525' ) {
-      setError( true )
+      setAlerta( true )
       setMensaje( '⛔️ Los datos son incorrectos!' )
       return
     } else {
-      alert( 'Sesión iniciada correctamente' )
-      setError( false )
-      setMensaje( '' )
+      setColor( 'alert alert-success' )
+      setAlerta( true )
+      setMensaje( 'Sesión iniciada correctamente' )
       setContrasena( '' )
       setNombre( '' )
-      setActive( false ) // Desactiva el botón
+      setActive( false )
     }
-
   }
 
   return (
     <div className="App container">
       <h2 className='mb-5'>Desafío estado de los componentes y eventos</h2>
       <form className='formulario' onSubmit={ validarDatos }>
-        { error && <p className='alert alert-danger'>{ mensaje }</p> }
+        { alerta && <p className={ color }>{ mensaje }</p> }
         <div className='form-group'>
           <label>Nombre</label>
           <input
